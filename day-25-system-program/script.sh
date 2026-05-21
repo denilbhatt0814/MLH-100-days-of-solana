5bdbcbecd059fa91a7aad0dba27440309b41158b
Denil Bhatt <denilbhatt2004@gmail.com>
2026-05-21 21:21:42 +0530

    DAY-25: comparing account data, for all sorts of accounts, wallets, system programs, system vars

---
+++ day-25-system-program/script.sh
@@ -0,0 +1,12 @@
+# inspect own wallet
+solana account $(solana address)
+
+# looking into native programs
+solana account 11111111111111111111111111111111
+solana account Stake11111111111111111111111111111111111111
+solana account Vote111111111111111111111111111111111111111
+
+# inspecting sys vars
+solana account SysvarRent111111111111111111111111111111111
+solana account SysvarC1ock11111111111111111111111111111111
+
