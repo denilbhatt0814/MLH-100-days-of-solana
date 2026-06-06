6552c4431c124cf7cdf9d79e32243e9f0e838584
Denil Bhatt <58869375+denilbhatt0814@users.noreply.github.com>
2026-06-06 12:44:33 +0000

    DAY-46: auditing NFT collection on chain

---
+++ day-46-audit-my-collection/script.sh
@@ -0,0 +1,45 @@
+spl-token display nftryjZd9LwC2d7cW75N5YiwypJyGZ1LA4kABRFJrWt
+# SPL Token Mint
+#   Address: nftryjZd9LwC2d7cW75N5YiwypJyGZ1LA4kABRFJrWt
+#   Program: TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb
+#   Supply: 1
+#   Decimals: 0
+#   Mint authority: (not set)
+#   Freeze authority: (not set)
+# Extensions
+#   Metadata Pointer:
+#     Authority: D8PsUTErJsVLTa3Wu3rtVvv9kwp4jm7sE4hH1LFKjRz1
+#     Metadata address: nftryjZd9LwC2d7cW75N5YiwypJyGZ1LA4kABRFJrWt
+#   Metadata:
+#     Update Authority: D8PsUTErJsVLTa3Wu3rtVvv9kwp4jm7sE4hH1LFKjRz1
+#     Mint: nftryjZd9LwC2d7cW75N5YiwypJyGZ1LA4kABRFJrWt
+#     Name: Brat the Simpson
+#     Symbol: SIMP
+#     URI: https://gist.githubusercontent.com/denilbhatt0814/89489e3cc20a5c9fd3639ec982d0f9e3/raw/be9e1ce0f101f35c8395c8cfc21c70218c341deb/day-44-metadata.json
+
+spl-token display BsTKbQ1Nmo8LqiWEApmiu6y4YQtGVoMZS2fC5TUizhb
+# SPL Token Mint
+#   Address: BsTKbQ1Nmo8LqiWEApmiu6y4YQtGVoMZS2fC5TUizhb
+#   Program: TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb
+#   Supply: 0
+#   Decimals: 0
+#   Mint authority: D8PsUTErJsVLTa3Wu3rtVvv9kwp4jm7sE4hH1LFKjRz1
+#   Freeze authority: (not set)
+# Extensions
+#   Metadata Pointer:
+#     Authority: D8PsUTErJsVLTa3Wu3rtVvv9kwp4jm7sE4hH1LFKjRz1
+#     Metadata address: BsTKbQ1Nmo8LqiWEApmiu6y4YQtGVoMZS2fC5TUizhb
+#   Group Pointer:
+#     Authority: D8PsUTErJsVLTa3Wu3rtVvv9kwp4jm7sE4hH1LFKjRz1
+#     Group address: BsTKbQ1Nmo8LqiWEApmiu6y4YQtGVoMZS2fC5TUizhb
+#   Metadata:
+#     Update Authority: D8PsUTErJsVLTa3Wu3rtVvv9kwp4jm7sE4hH1LFKjRz1
+#     Mint: BsTKbQ1Nmo8LqiWEApmiu6y4YQtGVoMZS2fC5TUizhb
+#     Name: Solana Sketchbook
+#     Symbol: SKTCH
+#     URI: https://gist.githubusercontent.com/janvinsha/b477ebe4dda46b0ef03895c4ea930a46/raw/f29222bcaff0d4979fe7ebb610a00bb97a8418ec/collection.json
+#   Token Group:
+#     Update Authority: D8PsUTErJsVLTa3Wu3rtVvv9kwp4jm7sE4hH1LFKjRz1
+#     Mint: BsTKbQ1Nmo8LqiWEApmiu6y4YQtGVoMZS2fC5TUizhb
+#     Size: 2
+#     Max Size: 3
