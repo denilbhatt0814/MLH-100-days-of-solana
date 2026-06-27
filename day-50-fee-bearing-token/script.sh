960a0313a519a4ad5b3dfcf9dc7e23e2480f10a7
Denil Bhatt <58869375+denilbhatt0814@users.noreply.github.com>
2026-06-27 08:46:36 +0000

    DAY-50: minting fee bearing tokens

---
+++ day-50-fee-bearing-token/script.sh
@@ -0,0 +1,36 @@
+
+spl-token --program-id TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb create-token --transfer-fee-basis-points 100 --transfer-fee-maximum-fee 1000000 --decimals 6
+# Creating token 78V9z1qaxv3N6BBZ11UwzsJRiVt35XczxoVTQVwPdZTz under program TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb
+
+# Address:  78V9z1qaxv3N6BBZ11UwzsJRiVt35XczxoVTQVwPdZTz
+# Decimals:  6
+
+# Signature: YzLT7thsZb6VpmAK7U7pzLzgZCnHqMbkcU9RDUmtVaGzXSotZWXWPMmTnnKraGCABQG2BVPhJEYRGS6KaGCzjXM
+
+spl-token create-account 78V9z1qaxv3N6BBZ11UwzsJRiVt35XczxoVTQVwPdZTz
+# Creating account CxP3aHCxVk5HGY4qqwrX2y7weJnm9armPL1SQQrELrVB
+
+# Signature: 5RXd68Sm6CMsUbGxTeEbdQBzehMQ2U85P3JejcmkpqfshLmyCowqVPYaukKihtE1HKgR2UU4AuUzkPsyFqgMtp96
+
+spl-token mint 78V9z1qaxv3N6BBZ11UwzsJRiVt35XczxoVTQVwPdZTz 1000
+# Minting 1000 tokens
+#   Token: 78V9z1qaxv3N6BBZ11UwzsJRiVt35XczxoVTQVwPdZTz
+#   Recipient: CxP3aHCxVk5HGY4qqwrX2y7weJnm9armPL1SQQrELrVB
+
+# Signature: 5ame6hxWZSmn3MH1uCvZ42epM8tEnbPGMiccNt8StLZm2MHhR3aTkX99tjffBbzD3cshvia1SLTkXQEGUNXyKRDg
+
+spl-token display 78V9z1qaxv3N6BBZ11UwzsJRiVt35XczxoVTQVwPdZTz
+# SPL Token Mint
+#   Address: 78V9z1qaxv3N6BBZ11UwzsJRiVt35XczxoVTQVwPdZTz
+#   Program: TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb
+#   Supply: 1000000000
+#   Decimals: 6
+#   Mint authority: D8PsUTErJsVLTa3Wu3rtVvv9kwp4jm7sE4hH1LFKjRz1
+#   Freeze authority: (not set)
+# Extensions
+#   Transfer fees:
+#     Current fee: 100bps
+#     Current maximum: 1000000000000
+#     Config authority: D8PsUTErJsVLTa3Wu3rtVvv9kwp4jm7sE4hH1LFKjRz1
+#     Withdrawal authority: D8PsUTErJsVLTa3Wu3rtVvv9kwp4jm7sE4hH1LFKjRz1
+#     Withheld fees: 0
